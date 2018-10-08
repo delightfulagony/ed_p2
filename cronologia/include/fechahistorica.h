c61b805f60ecee83c837b43017f4b3adc19b174f
agony <agony.root@protonmail.com>
2018-10-08 16:30:12 +0200

    Inicializo la clase fechaHistorica e inicializo la documentacion

---
+++ cronologia/include/fechahistorica.h
@@ -0,0 +1,101 @@
+/**
+ * @file fechahistorica.h
+ * @brief Fichero cabecera del TDA fechaHistorica
+ * @author Manuel Gachs Ballegeer
+ * @author Gonzalo Moreno Soto
+ * @license: GPLv3
+ */
+
+#ifndef __FECHAHISTORICA
+#define __FECHAHISTORICA
+
+/**
+ * @brief T.D.A. fechaHistorica
+ * Una instancia @e c del tipo de datos abstracto @c fechaHistorica es un objeto
+ * que representa a una fecha del calendario gregoriano junto con un conjunto de
+ * eventos ocurridos durante ese año
+ * @author Manuel Gachs Ballegeer
+ * @author Gonzalo Moreno Soto
+ * @date Octubre 2018
+ */
+class fechaHistorica {
+private:
+	/**
+	 * @page repConjunto Rep del TDA fechaHistorica
+	 * @section invConjunto Invariante de la representación
+	 * @section faConjunto Función de abstracción
+	 * Un objeto válido @e rep del TDA fechaHistorica representa al valor
+	 * (rep.ano,rep.nEventos,rep.*eventos)
+	 */
+
+	/**
+	 * @brief Año del objeto
+	 */
+	int ano;
+	/**
+	 * @brief Número de eventos en el año del objeto
+	 */
+	unsigned int nEventos;
+	/**
+	 * @brief Array de eventos
+	 */
+	std::string *eventos;
+
+	void reservarMemoria(const unsigned int _nEventos);
+	void liberarMemoria();
+	void copiar(const fechaHistorica& copia);
+public:
+	/**
+	 * @brief Constructor por defecto de fechaHistorica . Crea la fecha 
+	 * 1 D.C con 0 eventos
+	 */
+	fechaHistorica();
+	/**
+	 * @brief Constructor por copia del fechaHistorica
+	 * @param copia Objeto a copiar por el constructor
+	 */
+	fechaHistorica(const fechaHistorica& copia);
+	/**
+	 * @brief Constructor primitivo de fechaHistorica
+	 * @param _ano Año del objeto
+	 * @param _nEventos Número de eventos en el año del objeto
+	 */
+	fechaHistorica(const int& _ano, const unsigned int& _nEventos=0);
+	/**
+	 * @brief Destructor de la clase fechaHistorica
+	 */
+	~fechaHistorica();	
+	/**
+	 * @brief Observador del parámetro ano
+	 * @return Devuelve el año 
+	 */
+	int getAno() const {return ano;}
+	/**
+	 * @brief Observador de eventos
+	 * @param indice Indice del array de eventos
+	 * @return Evento en el lugar definido por indice
+	 */
+	std::string getEvento(const unsigned int& indice) const;
+	/**
+	 * @brief Altera el evento al que se refiere el índice, si no se
+	 * proporciona índice, se añade al final.
+	 * @param nuevo Evento a introducir en el objeto
+	 * @param indice Lugar en el que introducir el evento, si es -1 se añade
+	 * el evento al final.
+	 */
+	void asignarEvento(const std::string& nuevo, const int indice=-1);
+	/**
+	 * @brief Imprime la fecha en el formato "Año#Evento[1]#...#Evento[n]"
+	 */
+	void escribirFecha()const;
+	/**
+	 * @brief Suma al objeto otra fechaHistorica dada
+	 * @pre Que ambos objetos tengan el mismo año
+	 * @param sumaFechaHistorica objeto fechaHistorica a sumar
+	 * @return La suma de los eventos de ambas fechas
+	 */
+	fechaHistorica& suma(const fechaHistorica& sumaFechaHistorica);
+	bool buscarEvento(const std::string& _evento)const;
+};
+
+#endif
