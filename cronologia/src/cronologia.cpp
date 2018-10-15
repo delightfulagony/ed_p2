2fe1f2430600f122dcc78443d67e8c520324ac2d
agony <agony.root@protonmail.com>
2018-10-15 11:29:00 +0200

    Inicio implementacion de cronologia.cpp

---
+++ cronologia/src/cronologia.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file cronologia.h
+ * @brief Fichero cabecera del TDA cronologia
+ * @author Manuel Gachs Ballegeer
+ * @author Gonzalo Moreno Soto
+ * @license GPLv3
+ */
+
+#include "cronologia.h"
+
+void cronologia::reservarMemoria(const unsigned int% _nFechas) {
+
+}
