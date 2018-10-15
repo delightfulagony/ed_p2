993d06999d1ea9c2910f53fa717e20faa4de2e69
ManuelBelgicano <manuelgbal1@gmail.com>
2018-10-15 11:28:00 +0200

    Comienzo a implementar la clase fechahistorica

--- cronologia/src/fechahistorica.cpp
+++ cronologia/src/fechahistorica.cpp
@@ -1,7 +1,27 @@
-/*
- *
-*/
+/**
+ * @file fechahistorica.h
+ * @brief Fichero fuente del TDA fechaHistorica
+ * @author Manuel Gachs Ballegeer
+ * @author Gonzalo Moreno Soto
+ * @license: GPLv3
+ */
 
+#include "fechahistorica.h"
 #include <iostream>
 #include <string>
 
+void fechaHistorica::reservarMemoria(const unsigned int& _nEventos) {
+	if (eventos!=NULL)
+		liberarMemoria();
+	anio = 1;
+	nEventos = _nEventos;
+	eventos = new std::string[nEventos];
+}
+
+fechaHistorica::fechaHistorica() {
+	anio = 1;
+	nEventos = 0;
+	eventos = NULL;
+}
+
+fechaHistorica::fechaHistorica(const fechaHistorica& copia) {}
