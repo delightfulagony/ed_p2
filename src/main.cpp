e1e4c41a32523ee0fa73a05d2c5ba89edbac3e86
ManuelBelgicano <manuelgbal1@gmail.com>
2018-10-24 21:04:13 +0200

    Comienzo del menu en main

--- src/main.cpp
+++ src/main.cpp
@@ -13,14 +13,34 @@
 int main(int argc, char * argv[]) {
 	//Comprobación inicial
 	if (argc<2) {
-		ayuda();
+		std::cout<<"Error: Añada al menos un archivo de cronologia\n";
 		return -1;
 	}
 	
+	//Calculo de la cronologia total a partir de los parametros de entrada
+	cronologia crono;
+	for(size_t i=1;i<argc;i++)
+		crono += argv[i];
+	
+	//Rutina habitual del programa
 	char opt = 'q';
+	bool guardado = true;
 	do {
 		opt = menu();
-
+		switch(opt) {
+			case 's':
+				guardado = salvar(crono);
+				break;
+			case 'a':
+				mostrarEventos(crono);
+				break;
+			case 'm':
+				mostrarCronologia(crono);
+				break;
+			case 'u':
+				unirNueva(total,guardado);
+				break;
+		}
 	} while(opt!='q');
 	
 	return 0;
