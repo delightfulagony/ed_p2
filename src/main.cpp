714311dcb8b0c232972ccad6e568098455d37bc0
ManuelBelgicano <manuelgbal1@gmail.com>
2018-10-24 19:33:26 +0200

    Aplico cambios recientes a main

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,7 @@
 #include <iostream>
 
 int main(int argc, char * argv[]) {
-	//Comprobaciones iniciales
-	bool opcion = false;
-	bool opcion_add = false;
-	bool opcion_exitfile = false;
+	//Comprobación inicial
 	if (argc<2) {
 		ayuda();
 		return -1;
@@ -26,12 +23,5 @@ int main(int argc, char * argv[]) {
 
 	} while(opt!='q');
 	
-   /* Exhibir aquí la funcionalidad programada para el TDA Cronologia / TDA FechaHistorica */ 
-
-   // Algunas sugerencias: 
-   // - Obtener los eventos acaecidos en un año dado
-   // - Obtener la (sub)cronología de eventos históricos acaecidos en [anioDesde, anioHasta], donde anioDesde y anioHasta son proporcionados por el usuario
-   // - Obtener la (sub)cronología de eventos históricos asociados a una palabra clave. Ejemplo: la cronología de eventos asociados a "Einstein"
-   // - Operadores sobre cronologías, ejemplo: Unión de dos cronologías (la cronología resultante debería estar ordenada)
-   // - Cualquier otra funcionalidad que consideréis de interés
+	return 0;
 }
