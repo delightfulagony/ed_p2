2365df1879d5c1acf0eaf8529f902e9c082f21e0
ManuelBelgicano <manuelgbal1@gmail.com>
2018-10-24 17:49:46 +0200

    Empiezo el main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
+/**
+ * @file main.cpp
+ * @brief Fichero fuente del programa
+ * @author Manuel Gachs Ballegeer
+ * @author Gonzalo Moreno Soto
+ * @license GPLv3
+ */
 #include "cronologia.h"
+#include "auxiliares.h"
 #include <fstream>
 #include <iostream>
 
@@ -8,19 +16,15 @@
 void ayuda();
 
 int main(int argc, char * argv[]) {
-
-	if (argc!=2) {
-		cout<<"Error en el número de argumentos\n";
+	//Comprobaciones iniciales
+	bool opcion = false;
+	bool opcion_add = false;
+	bool opcion_exitfile = false;
+	if (argc<2) {
 		ayuda();
 		return -1;
 	}
-
-	ifstream f(argv[1]);
-	if (!f) {
-	cout<<"No puedo abrir el fichero "<<argv[1]<<endl;
-	return -1;
-	}
-
+	
    /* Exhibir aquí la funcionalidad programada para el TDA Cronologia / TDA FechaHistorica */ 
 
    // Algunas sugerencias: 
@@ -29,16 +33,15 @@ int main(int argc, char * argv[]) {
    // - Obtener la (sub)cronología de eventos históricos asociados a una palabra clave. Ejemplo: la cronología de eventos asociados a "Einstein"
    // - Operadores sobre cronologías, ejemplo: Unión de dos cronologías (la cronología resultante debería estar ordenada)
    // - Cualquier otra funcionalidad que consideréis de interés
-
-
 }
 
 void ayuda {
-	std::cout<<"__________________________________CRONOLOGIA__________________________________\n";
+	std::cout<<"_________________________________C R O N O L O G I A_________________________________\n";
 	std::cout<<"Uso del programa:\n";
-	std::cout<<"		./cronologia (opcion) [arg]\n";
-	std::cout<<"Lista de opciones:\n";
-	std::cout<<"	--help		Muestra este mensaje de ayuda\n";
-	std::cout<<"	--add		Añade los argumentos a una cronología ya dada como argumento\n";
-	std::cout<<"	--\n"
+	std::cout<<"./cronologia [-h|-a ficheroCronologia|-f ficheroSalida] fichero1.txt fichero2.txt ...\n";
+	std::cout<<"	-h			   Muestra este mensaje de ayuda\n";
+	std::cout<<"	-a FicheroCronologia	   Añade las nuevas cronologías a la de FicheroCronologia\n";
+	std::cout<<"	-f FicheroSalida	   Guarda las cronologías a FicheroSalida\n";
+	std::cout<<"	fichero*.txt		   Ficheros con las cronologías con las que trabajar\n";
+	std::cout<<"_____________________________________________________________________________________\n";
 }
